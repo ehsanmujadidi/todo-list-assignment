82d61e986a4bc55c14315773feb7fcccbc14ca2e
Muhammad Ehsan Mujadidi <ehsan.mujadidi@gmail.com>
2022-06-14 22:02:44 -0500

    new changes applied

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React from 'react';
 
+/**
+ * Todo list array
+ */
+const todoList = [
+  {
+    id: 1,
+    title: "First Assignment" 
+  },
+  {
+    id: 2,
+    title: "Second Assignment" 
+  },
+  {
+    id: 3,
+    title: "Third Assignment" 
+  }
+];
+
+/**
+ * App component
+ */
 function App() {
+  /**
+   * map array inside the <li>
+   */
+  const listItems = todoList.map(function(item){
+    return (
+      <li key={item.id}>
+        {item.title}
+      </li>
+    );
+  })
+  
+  /**
+   * Return result
+   */
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {listItems}
+      </ul>
     </div>
   );
 }
